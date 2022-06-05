8f6dab616cb62889aef64f672590f281d38cd72f
kanlee <kanlee@student.42seoul.kr>
2022-06-05 11:22:29 +0900

    Use std::allocator instead of new-delete. Add push_back(), pop_back(), swap(), clear(), get_allocator().

--- vector.hpp
+++ vector.hpp
@@ -3,6 +3,7 @@
 
 # include <memory>
 # include <limits>
+# include <algorithm>
 
 namespace ft {
 
@@ -25,31 +26,39 @@ class vector {
 		typedef size_t				size_type;
 
 	private:
-		T			*m_array;
-		size_type	m_size;
-		size_type	m_capacity;
+		T				*m_array;
+		size_type		m_size;
+		size_type		m_capacity;
+		allocator_type	m_alloc;
+
 
 	public:
 		// Ctor
 		vector(const allocator_type& alloc = allocator_type()) : m_size(0), m_capacity(0) {}							// default
 		vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) {		// fill
-
-			m_array = new T[n];
+//			m_array = new T[n];
+			m_array = m_alloc.allocate(n);
 			m_size = n;
 			m_capacity = n;
 			for (size_t i = 0; i < n; i++)
 				m_array[i] = val;
 		}
+/*
+		vec(20, 0); 의 경우 fill이 아닌 range가 선택될 수 있기 때문에 enable_if 구현이 필요하다.
 		template <typename InputIterator>
 		vector(InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()) {}				// range
+*/
+
 		// Dtor
 		~vector() {
 			if (m_capacity)
-				delete[] m_array;
+				// delete[] m_array;
+				m_alloc.deallocate(m_array, m_capacity);
 		}
 		// Copy Ctor
 		vector(const vector& other) {
-			m_array = new T[other.m_capacity];
+//			m_array = new T[other.m_capacity];
+			m_array = m_alloc.allocate(other.m_capacity);
 			m_size = other.m_size;
 			m_capacity = other.m_capacity;
 			for (size_t i = 0; i < m_size; i++)
@@ -75,14 +84,15 @@ class vector {
 
 		// capacity
 		size_type		size() const { return m_size; }
+
 		/* returns the maximum number of elements that vector can hold.
 		 * This is the maximum potential size the container can reach due to system or implementation limitations.
 		 * container isn't guaranteed to to be able to reach that size: it can still fail to allocate before that size is reached.
 		 */
-
 		size_type		max_size() const {
 			return std::numeric_limits<difference_type>::max() / sizeof(T);
 		}
+
 		/* container will contain n elements
 		 * if n is smaller than current size, contents are reduced to n elements, and removing those beyond.
 		 * if n is greater than current size, capacity is expanded to n, and new elements are initialized as val.
@@ -97,7 +107,7 @@ class vector {
 						tmp[i] = m_array;
 					else
 						tmp[i] = val;
-				swap(*this, tmp);
+				swap(tmp);
 				return;
 			}
 			else
@@ -107,8 +117,11 @@ class vector {
 				size = n;
 			}
 		}
+
 		size_type		capacity() const { return m_capacity; }
+
 		bool			empty() const { return m_size == 0; }
+
 		/* capacity will be at least n.
 		 * if n is greater than current capacity, automatic reallocation will accur.
 		 * otherwise, do nothing.
@@ -117,9 +130,10 @@ class vector {
 		void			reserve(size_type n) {
 			if ( n > m_capacity) {
 				vector tmp(n);
+				tmp.m_size = m_size;
 				for (size_t i = 0; i < m_size; i++)
 					tmp.m_array[i] = m_array[i];
-				swap(*this, tmp);
+				swap(tmp);
 			}
 		}
 
@@ -138,30 +152,59 @@ class vector {
 		template <typename InputIterator>
 		void			assign(InputIterator first, InputIterator last);	// range
 		void 			assign(size_type n, const value_type& val);			// fill
-		void			push_back(const value_type& val);
-		void			pop_back();
+#endif
+#if 1
+
+		void			push_back(const value_type& val) {
+			if (m_size >= m_capacity)
+				reserve((m_capacity == 0) ? 1 : m_capacity * 2);
+			m_array[m_size++] = val;
+		}
+
+		void			pop_back() {
+			if (m_size) {
+				m_alloc.destroy(m_array + m_size - 1);
+//				m_array[--m_size].value_type::~value_type();
+				m_size--;
+			}
+		}
+#endif
+#if 0
 		iterator		insert(iterator position, const value_type& val);					// single element
 		void			insert(iterator position, size_type n, const value_type& val);		// fill
 		template <typename InputIterator>
 		void			isert(iterator position, InputIterator first, InputIterator last);	//range
 		iterator		erase(iterator position);
 		iterator		erase(iterator first, iterator last);
-		void			swap(vector& x);
-		void			clear();
+#endif
+		void			swap(vector& x) {
+			std::swap(m_array, x.m_array);
+			std::swap(m_size, x.m_size);
+			std::swap(m_capacity, x.m_capacity);
+			std::swap(m_alloc, x.m_alloc);
+		}
+
+		void			clear() {
+			while (m_size)
+				pop_back();
+		}
 
 		// Allocator
-		allocator_type	get_allocator() const;
-
-		// relational operators (==, !=, <, <=, >, >=)
-
-		// swap
-		template <typename T, Typename Alloc>
-		void			swap(vector<T, Alloc>& x, vector<T, Alloc>& y);
-#endif
-
-
+		allocator_type	get_allocator() const {
+			return m_alloc;
+		}
 
 };
+  
+// relational operators (==, !=, <, <=, >, >=)
+
+// non-member function overloads
+template <typename T, typename Alloc>
+void swap(vector<T, Alloc>& x, vector<T, Alloc>& y) {
+	x.swap(y);
+}
+
+
 
 }
 #endif
